3c0b1cff50f8455c6092653d9bfe67ef79ebf635
acharayaP03 <acharyaP03@gmail.com>
2024-10-08 14:17:56 +1100

    use concurrenty to build and compile ts files

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,32 @@
-import fs from "fs";
+import { CsvReader } from "./FileReader/CsvReader.ts";
 
-const matchers = fs
-  .readFileSync("football.csv", "utf8")
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const csvReader = new CsvReader("football.csv");
+csvReader.read();
 
-let manUnitedWins = 0;
-
-for (let match of matchers) {
-  if (match[1] === "Man United" && match[5] === "H") {
-    manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
-    manUnitedWins++;
-  }
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
 }
+
+// let manUnitedWins = 0;
+
+// for (let match of matchers) {
+//   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+//     manUnitedWins++;
+//   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+//     manUnitedWins++;
+//   }
+// }
+
+const manUnitedWins = csvReader.data.reduce((acc, match) => {
+  if (
+    (match[1] === "Man United" && match[5] === MatchResult.HomeWin) ||
+    (match[2] === "Man United" && match[5] === MatchResult.AwayWin)
+  ) {
+    return acc + 1;
+  }
+  return acc;
+}, 0);
 
 console.log(`Man United won ${manUnitedWins} games`);
